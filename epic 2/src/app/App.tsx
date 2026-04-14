521bae041fe614388334fb994fd3608ae9ce3c24
Alan Leemon <alanleemon@Alans-MacBook-Pro.local>
2026-04-14 12:10:39 +1000

    integrated epic2, deleted unwanted files

--- epic 2/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import BiodiversityDashboard from './components/BiodiversityDashboard';
-
-export default function App() {
-  return (
-    <div className="size-full">
-      <BiodiversityDashboard />
-    </div>
-  );
-}
